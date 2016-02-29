bdaf28a865c65bbabde72c6cc69005d8591406d7
Rohan Bhatia <rohan1413@gmail.com>
2016-02-29 12:29:21 -0500

    added sta248_pqset3.r

--- sta248_pqset3.r
+++ sta248_pqset3.r
@@ -3,5 +3,12 @@
 
 ch7q27 <- function() {
 
-	#x = c(319, 338, 337, 339, 328, 325, 340, 331, 341)
+	print("Question 27")
+	print("A: Plot data in stem and leaf diagram. Reasonable to assume data is normally distrbuted? Explain")
+	x = c(319, 338, 337, 339, 328, 325, 340, 331, 341, 336, 330, 330, 321, 327, 337, 320, 343, 350, 322, 334, 326, 349, 341, 338, 332, 339, 335, 338, 333, 334)
+	stem(x)
+	print("Yes, as the shape of the stem and leaf diagram suggests this")
 }  
+
+
+ch7q27()
