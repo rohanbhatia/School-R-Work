34458402483f22e8b486e43c4768198fe1692f3e
Rohan Bhatia <rohan1413@gmail.com>
2016-03-23 15:59:00 -0400

    finished chp8q13

--- sta248_pqset5.r
+++ sta248_pqset5.r
@@ -49,5 +49,96 @@ ch8q1 <- function() {
 
 }
 
+ch8q3 <- function() {
 
-ch8q1()
+	x = c(21.9, 23.4, 22.1, 22.1, 24.7, 24.6, 24.0, 24.1, 24.2, 26.5, 23.8, 25.3, 24.8, 24.8, 24.5, 27.8, 24.9, 27.2, 25.1, 25.5, 23.7, 26.5, 22.0, 26.7, 25.2, 23.1, 25.4)
+
+	print("Part A")
+	stem(x)
+	print("Yes, assumption of normality appears reasonable")
+
+	print("Part B")
+	#sample mean
+	n = length(x)
+	xbar = sum(x) / n
+
+	#sample variance calculation
+	diff = (x - xbar)*(x-xbar)
+	s2 = sum(diff) / (n-1)
+	print(s2)
+
+	print("Part C")
+	#confidence interval calculation
+	c_i = 99
+	calc_1 = 100 - c_i
+	alpha = calc_1/100
+	alpha_div_2 = alpha/2
+	one_min_alpha_div_2 = 1 - alpha_div_2
+	df = n-1
+	chi_sq_alpha_div_2 = qchisq(alpha_div_2, df)
+	chi_sq_one_min_alpha_div_2 = qchisq(one_min_alpha_div_2, df)
+	l2 = (df*s2)/chi_sq_alpha_div_2
+	l1 = (df*s2)/chi_sq_one_min_alpha_div_2
+	print("L1, L2")
+	print(l1)
+	print(l2)
+
+	print("Part D")
+	#stdev CI calc
+	newl1 = sqrt(l1)
+	newl2 = sqrt(l2)
+	print("L1, L2")
+	print(newl1)
+	print(newl2)
+	print("Try 95% CI")
+	c_i = 80
+	calc_1 = 100 - c_i
+	alpha = calc_1/100
+	alpha_div_2 = alpha/2
+	one_min_alpha_div_2 = 1 - alpha_div_2
+	df = n-1
+	chi_sq_alpha_div_2 = qchisq(alpha_div_2, df)
+	chi_sq_one_min_alpha_div_2 = qchisq(one_min_alpha_div_2, df)
+	l2 = (df*s2)/chi_sq_alpha_div_2
+	l1 = (df*s2)/chi_sq_one_min_alpha_div_2
+	print("L1, L2")
+	print(l1)
+	print(l2)
+
+}
+
+ch8q13 <- function() {
+
+x = c(2.0, 1.4, 3.5, 2.3, 3.2, 3.6, 0.1, 3.5, 2.2, 2.1, 2.4, 1.5, 2.2, 2.3, 2.7, 1.9, 1.7, 1.8, 3.1, 1.5, 1.5, 2.6, 2.8, 2.5, 2.5, 3.9, 0.8, 1.8, 3.3, 3.7)
+
+print("Part A")
+#sample mean
+print("Sample Mean")
+n = length(x)
+xbar = sum(x) / n
+print(xbar)
+#sample variance calculation
+print("Sample Variance")
+diff = (x - xbar)*(x-xbar)
+s2 = 0.89#sum(diff) / (n-1)
+print(s2)
+
+
+print("Part B")
+df = n-1
+t = qt(0.995, df)
+l = t*(s2/sqrt(n))
+print("L1, L2 are xbar +- :")
+print(l)
+
+print("Part C")
+print("yes - we are 99% confident that x is within 2.35 +- 0.45. 6.6 is out of this range")
+
+}
+
+
+ch8q13()
+
+
+
+
