b77b95286023194cfad739f9b59c08a253641bb1
Rohan Bhatia <rohan1413@gmail.com>
2016-04-20 19:57:59 -0400

    completed ch7q49

--- sta248_pqset4.r
+++ sta248_pqset4.r
@@ -5,8 +5,8 @@ ch7q47 <- function() {
 
 	x = c(0.645, 0.654, 0.640, 0.627, 0.626, 0.649, 0.629, 0.631, 0.643, 0.633, 0.646, 0.630, 0.634, 0.631, 0.651, 0.659, 0.638, 0.645, 0.655, 0.624, 0.658, 0.658, 0.658, 0.647, 0.665)
 
+	print("Part A")
 	xbar = sum(x) / length(x)
-
 	print("n")
 	print(length(x))
 	print("Sample mean")
@@ -45,4 +45,30 @@ ch7q47 <- function() {
 	l3 = (zalpha23*0.01)/5
 	print(l3)
 }
+
+ch7q49 <- function() {
+	
+x = c(3, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4, 3, 2, 3, 3, 2, 3, 3, 3, 3, 3, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 2, 3, 3, 3, 3, 3)
+
+print("Part A")
+print("No, it is discrete and thus cannot be normal")
+
+print("Part B")
+print(mean(x))
+
+print("Part C")
+print(mean(x))
+print("+-")
+ci = qnorm(0.995, mean=0, sd=1) * 0.5 * (1/sqrt(40))
+print(ci)
+print("CLT")
+
+print("Part D")
+print("No, as 3.0 is in the 99% CI based on the data")
+
+
+}
+
+
+
 
