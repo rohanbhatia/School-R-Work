a3e7d5b39a0b410b4f720b6f1bf458a8ace8a4cf
Rohan Bhatia <rohan1413@gmail.com>
2016-04-21 13:06:29 -0400

    updated q1

--- sta248_pqset5.r
+++ sta248_pqset5.r
@@ -2,49 +2,35 @@
 
 ch8q1 <- function() {
 
-	x = c(1.48, 1.26, 1.52, 1.56, 1.48, 1.46, 1.30, 1.28, 1.43, 1.43, 1.55, 1.57, 1.51, 1.53, 1.68, 1.37, 1.47, 1.61, 1.49, 1.43, 1.64, 1.51, 1.60, 1.65, 1.60, 1.64, 1.51, 1.51, 1.53, 1.74)
+x = c(1.48, 1.26, 1.52, 1.56, 1.48, 1.46, 1.30, 1.28, 1.43, 1.43, 1.55, 1.57, 1.51, 1.53, 1.68, 1.37, 1.47, 1.61, 1.49, 1.43, 1.64, 1.51, 1.60, 1.65, 1.60, 1.64, 1.51, 1.51, 1.53, 1.74)
 
-	print("Part A")
-	stem(x)
-	print("Yes, assumption of normality appears reasonable")
+print("Part A")
+stem(x)
+print("yes")
 
-	print("Part B")
-	#mean
-	n = length(x)
-	xbar = sum(x) / n
+print("Part B")
+print("Use sample variance")
+n = length(x)
+xbar = mean(x)
+s2 = sum((x - xbar)^2)/(n - 1)
+print(s2)
 
-	#sample variance calculation
-	diff = (x - xbar)*(x-xbar)
-	s2 = sum(diff) / (n -1)
-	print(s2)
+print("Part C")
+chisq_alpha_div_two = 45.722
+chisq_one_min_alpha_div_two = 16.047
+l1 = ((n-1)*(s2))/chisq_alpha_div_two
+l2 = ((n-1)*(s2))/chisq_one_min_alpha_div_two
+print(l1)
+print(l2)
 
-	print("Part C")
-	#confidence interval calculation
-	c_i = 95
-	calc_1 = 100 - c_i
-	alpha = calc_1/100
-	alpha_div_2 = alpha/2
-	one_min_alpha_div_2 = 1 - alpha_div_2
-	df = n-1
-	chi_sq_alpha_div_2 = qchisq(alpha_div_2, df)
-	chi_sq_one_min_alpha_div_2 = qchisq(one_min_alpha_div_2, df)
-	l2 = (df*s2)/chi_sq_alpha_div_2
-	l1 = (df*s2)/chi_sq_one_min_alpha_div_2
-	print("L1, L2")
-	print(l1)
-	print(l2)
+print("Part D")
+l12 = sqrt(l1)
+l22 = sqrt(l2)
+print(l12)
+print(l22)
 
-	print("Part D")
-	#stdev CI calc
-	newl1 = sqrt(l1)
-	newl2 = sqrt(l2)
-	print("L1, L2")
-	print(newl1)
-	print(newl2)
-
-	print("Part E")
-	#value of 0.2 surprising?
-	print("Yes, because 0.2 is not in the CI from part D")
+print("Part E")
+print("yes, it is outside the CI")
 
 
 }
